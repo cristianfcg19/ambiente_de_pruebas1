8152a39e97967ff40d1219b124ff5852d12c44ac
cristianfcg19 <102130391+cristianfcg19@users.noreply.github.com>
2022-06-16 18:09:51 -0600

    cambio 16/6/2022

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const {Builder,By,Key,util}=require("selenium-webdriver");
 async function ejemplo()
 {
 let driver=await new Builder().forBrowser("firefox").build();
-await driver.get("file:///C:/Users/Christian/Documents/GitHub/website-templates/amaze-photography-bootstrap-html5-template/index.html");
+await driver.get("file:///C:/Users/Christian/Documents/GitHub/EcommerceTemplateBootstrap/index.html");
 await driver.findElement(By.name("hola").sendKeys("Selenium" ,Key.RETURN));
 }
 
