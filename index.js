0e924fc4494288645bbab2ef44f7751e28034490
cristianfcg19 <102130391+cristianfcg19@users.noreply.github.com>
2022-08-10 22:36:42 -0600

    cree la Usuariovalidacion

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const {Builder,By,Key,util}=require("selenium-webdriver");
 
- async function CP_U2()
+ async function CP_U2_Crear()
 {
 let driver=await new Builder().forBrowser("firefox").build();
 ///Esta es URL QUE VA ABRIR
@@ -45,7 +45,54 @@ else
 }
 }
 
-CP_U2();
+
+async function CP_U2Validar()
+{
+let driver=await new Builder().forBrowser("firefox").build();
+///Esta es URL QUE VA ABRIR
+await driver.get("https://maniruzzamanakash.github.io/EcommerceTemplateBootstrap/#");
+///LO SIGUIENTE  ES PARA ABRIR LA LISTA Y OPRIMIR LINK VIEW PERSONAL 
+await driver.findElement(By.xpath(`//*[@id="top"]/div[3]/div/div[3]/div/nav/ul/li[6]/a`)).click();
+await driver.findElement(By.xpath(`//*[@id="top"]/div[3]/div/div[3]/div/nav/ul/li[6]/a`)).click();
+await driver.findElement(By.xpath(`//*[@id="top"]/div[3]/div/div[3]/div/nav/ul/li[6]/a`)).click();
+await driver.findElement(By.css(".wsmenu-submenu > li:nth-child(1) > a:nth-child(1) > i:nth-child(1)")).click();
+//el getcurrente Es para actualizar la URl ala pagina que esta en ese momento
+await driver.getCurrentUrl();
+//HACE CLICK EN EL INFORMACION PERSONAL
+await driver.findElement(By.xpath(`/html/body/div/div[2]/div/div/div/div[1]/ul/li[2]/a`)).click();
+//Actualiza otra vez la url
+await driver.getCurrentUrl();
+///Lo siguiente limpia el los campos
+await driver.findElement(By.xpath(`//*[@id="firstname"]`)).clear();
+await driver.findElement(By.xpath(`//*[@id="lastname"]`)).clear();
+await driver.findElement(By.xpath(`//*[@id="email"]`)).clear();
+await driver.findElement(By.xpath(`//*[@id="username"]`)).clear();
+await driver.findElement(By.className("form-control")).clear();
+///lo siguiente agrega texto alos campos
+await driver.findElement(By.xpath(`//*[@id="firstname"]`)).sendKeys("Christian");
+
+await driver.findElement(By.xpath(`//*[@id="email"]`)).sendKeys("cristian12cgmail.com");
+
+await driver.findElement(By.xpath(`//*[@id="birthdate"]`)).click();
+await driver.findElement(By.xpath(`//*[@id="submitForm"]`)).click();//Da click en save
+
+///Valida si hay un error 
+if(await driver.findElement(By.xpath(`//*[@id="email-error"]`)).getText()=="Please enter a valid email address"
+&& await driver.findElement(By.xpath(`//*[@id="lastname-error"]`)).getText()=="Please enter your lastname"&& 
+await driver.findElement(By.xpath(`//*[@id="username-error"]`)).getText()=="Please enter a username")
+{
+    console.log( await driver.findElement(By.xpath(`//*[@id="email-error"]`)).getText());
+    console.log(await driver.findElement(By.xpath(`//*[@id="lastname-error"]`)).getText());
+    console.log(await driver.findElement(By.xpath(`//*[@id="username-error"]`)).getText());
+}
+else
+{
+    console.log("Todo Salio Correctamente");
+}
+}
+
+//CP_U2_Crear();
+CP_U2Validar();
 
 
 
