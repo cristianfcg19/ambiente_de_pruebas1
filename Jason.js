eec3e0cca779376eda60e5ebecb77c9447b2b44c
Jason Quiros <jquiros90650@ufide.ac.cr>
2022-08-11 19:10:54 -0600

    Codigo

--- Jason.js
+++ Jason.js
@@ -4,30 +4,37 @@ async function CP_U1_02(){
 let driver=await new Builder().forBrowser("firefox").build();
 await driver.get("https://maniruzzamanakash.github.io/EcommerceTemplateBootstrap/index.html")
 
- //Comprobacion de Correo sin arroba
- 
-  await driver.findElement(By.xpath('/html/body/div/div[3]/div/div/div[2]/div[1]/form/input[1]')).sendKeys("Proyecto.com")
-  await driver.findElement(By.xpath('/html/body/div/div[3]/div/div/div[2]/div[1]/form/input[2]')).click();
+   //Comprobacion de Correo sin arroba
+   
+    await driver.findElement(By.xpath('/html/body/div/div[3]/div/div/div[2]/div[1]/form/input[1]')).sendKeys("Proyecto.com");
+    await driver.findElement(By.xpath('/html/body/div/div[3]/div/div/div[2]/div[1]/form/input[2]')).click();
+    await driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS)
+    await assertEquals("405 Not Allowed",driver.getTitle());
 
 
    //Comprobacion de Correo sin .com
  
    await driver.findElement(By.xpath('/html/body/div/div[3]/div/div/div[2]/div[1]/form/input[1]')).sendKeys("Prueba")
    await driver.findElement(By.xpath('/html/body/div/div[3]/div/div/div[2]/div[1]/form/input[2]')).click();
-
+   await driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS)
+   await assertEquals("405 Not Allowed",driver.getTitle());
 
  //Comprobacion de Correo con numeros 
  
  await driver.findElement(By.xpath('/html/body/div/div[3]/div/div/div[2]/div[1]/form/input[1]')).sendKeys("1234567890")
  await driver.findElement(By.xpath('/html/body/div/div[3]/div/div/div[2]/div[1]/form/input[2]')).click();
-
+ await driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS)
+ await assertEquals("405 Not Allowed",driver.getTitle());
  
  //Comprobacion de Correo sin datos
  
  await driver.findElement(By.xpath('/html/body/div/div[3]/div/div/div[2]/div[1]/form/input[1]')).sendKeys("   ")
  await driver.findElement(By.xpath('/html/body/div/div[3]/div/div/div[2]/div[1]/form/input[2]')).click();
+ await driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS)
+ await assertEquals("405 Not Allowed",driver.getTitle());
 
 }
+
 CP_U1_02();
 
 
