13e414c37b01ad64cef307d03c3daab82850bcd2
Jason Quiros <jquiros90650@ufide.ac.cr>
2022-08-22 17:38:35 -0600

    last update

--- Jason.js
+++ Jason.js
@@ -2,36 +2,31 @@ const {Builder,By,Key,util}=require("selenium-webdriver");
 
 async function CP_U1_02(){
 let driver=await new Builder().forBrowser("firefox").build();
-await driver.get("https://maniruzzamanakash.github.io/EcommerceTemplateBootstrap/index.html")
+await driver.get("https://maniruzzamanakash.github.io/EcommerceTemplateBootstrap/index.html");
 
    //Comprobacion de Correo sin arroba
    
     await driver.findElement(By.xpath('/html/body/div/div[3]/div/div/div[2]/div[1]/form/input[1]')).sendKeys("Proyecto.com");
     await driver.findElement(By.xpath('/html/body/div/div[3]/div/div/div[2]/div[1]/form/input[2]')).click();
-    await driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS)
-    await assertEquals("405 Not Allowed",driver.getTitle());
-
+   
+   
 
    //Comprobacion de Correo sin .com
  
-   await driver.findElement(By.xpath('/html/body/div/div[3]/div/div/div[2]/div[1]/form/input[1]')).sendKeys("Prueba")
-   await driver.findElement(By.xpath('/html/body/div/div[3]/div/div/div[2]/div[1]/form/input[2]')).click();
-   await driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS)
-   await assertEquals("405 Not Allowed",driver.getTitle());
+   //await driver.findElement(By.xpath('/html/body/div/div[3]/div/div/div[2]/div[1]/form/input[1]')).sendKeys("Prueba");
+   //await driver.findElement(By.xpath('/html/body/div/div[3]/div/div/div[2]/div[1]/form/input[2]')).click();
+  
 
  //Comprobacion de Correo con numeros 
  
- await driver.findElement(By.xpath('/html/body/div/div[3]/div/div/div[2]/div[1]/form/input[1]')).sendKeys("1234567890")
- await driver.findElement(By.xpath('/html/body/div/div[3]/div/div/div[2]/div[1]/form/input[2]')).click();
- await driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS)
- await assertEquals("405 Not Allowed",driver.getTitle());
+ //await driver.findElement(By.xpath('/html/body/div/div[3]/div/div/div[2]/div[1]/form/input[1]')).sendKeys("1234567890");
+ //await driver.findElement(By.xpath('/html/body/div/div[3]/div/div/div[2]/div[1]/form/input[2]')).click();
  
  //Comprobacion de Correo sin datos
  
- await driver.findElement(By.xpath('/html/body/div/div[3]/div/div/div[2]/div[1]/form/input[1]')).sendKeys("   ")
- await driver.findElement(By.xpath('/html/body/div/div[3]/div/div/div[2]/div[1]/form/input[2]')).click();
- await driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS)
- await assertEquals("405 Not Allowed",driver.getTitle());
+ //await driver.findElement(By.xpath('/html/body/div/div[3]/div/div/div[2]/div[1]/form/input[1]')).sendKeys("   ");
+// await driver.findElement(By.xpath('/html/body/div/div[3]/div/div/div[2]/div[1]/form/input[2]')).click();
+
 
 }
 
